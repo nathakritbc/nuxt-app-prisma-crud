023e104c4bf0b55c8061e369cd633f27c107b623
Nathakrit Thitsatian <63659324+nathakritbc@users.noreply.github.com>
2024-05-31 21:54:09 +0700

    commit preview

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,68 +2,82 @@
 import { ref } from "vue";
 import { useDisplay } from "vuetify";
 const drawer = ref(true);
-const rail = ref(false);
+const rail = ref(true);
 const { name } = useDisplay();
 watchEffect(() => {
   rail.value = false;
-  if (name.value == "xs" || name.value == "sm") {
+  if (name.value == "xs") {
     rail.value = true;
   }
 });
+// onMounted(() => {
+//   drawer.value = true;
+//   if (name.value == "xs") {
+//     drawer.value = false;
+//   }
+// });
 </script>
 
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      :rail="rail"
-      temporary
-      permanent
-      @click="rail = false"
-    >
-      <v-list-item
-        prepend-avatar="../assets/images/profile.png"
-        title="John Leider"
-        :class="['px-3 py-4 ', name !== 'xs' ? 'border-b' : '']"
-        nav
+    <ClientOnly>
+      <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        temporary
+        permanent
+        @click.stop="rail = !rail"
       >
-        <template v-slot:append>
-          <v-btn
-            icon="mdi-chevron-left"
-            variant="text"
-            @click.stop="rail = !rail"
-          ></v-btn>
-        </template>
-      </v-list-item>
+        <v-list-item
+          prepend-avatar="../assets/images/profile.png"
+          title="John Leider"
+          :class="[
+            'px-3 py-4 cursor-pointer ',
+            name !== 'xs' ? '' : 'border-b',
+          ]"
+          nav
+        >
+          <v-tooltip activator="parent" location="left">โปรไฟล์</v-tooltip>
+          <template v-slot:append>
+            <v-btn
+              icon="mdi-chevron-left"
+              variant="text"
+              @click.stop="rail = !rail"
+            ></v-btn>
+          </template>
+        </v-list-item>
 
-      <v-list density="compact" nav class="px-2 mt-1">
-        <v-list-item
-          prepend-icon="mdi-home-city"
-          title="Home"
-          @click="$router.push('/')"
-          value="home"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-account"
-          title="My Account"
-          value="account"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-list-box"
-          @click="$router.push('/products')"
-          title="Stock"
-          value="stock"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-account-group-outline"
-          title="Users"
-          value="users"
-        ></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+        <v-list density="compact" nav class="px-2 mt-1">
+          <v-list-item
+            prepend-icon="mdi-home-city"
+            title="หน้าหลัก"
+            value="home"
+          >
+            <v-tooltip activator="parent" location="left">หน้าหลัก</v-tooltip>
+          </v-list-item>
+          <v-list-item
+            prepend-icon="mdi-account"
+            title="My Account"
+            value="account"
+          >
+            <v-tooltip activator="parent" location="left">โปรไฟล์</v-tooltip>
+          </v-list-item>
+          <v-list-item
+            prepend-icon="mdi-list-box"
+            @click="$router.push('/products')"
+            title="Stock"
+            value="stock"
+          >
+            <v-tooltip activator="parent" location="left"
+              >รายการสินค้า</v-tooltip
+            >
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </ClientOnly>
     <v-app-bar color="">
       <template v-slot:prepend v-if="name === 'xs'">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon @click.stop="rail = !rail"></v-app-bar-nav-icon>
       </template>
 
       <!-- <v-app-bar-title>Title</v-app-bar-title> -->
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
+<script lang="ts" setup>
+import Products from "./products/index.vue";
+</script>
+
 <template>
   <div>
-    <v-btn>Button</v-btn>
-    <h1 class="text-xl font-bold underline">Hello world!</h1>
-    <h1>Welcome to the homepage ไไทย </h1>
-    <h1>
-      <div class="text-2xl ">ไทย ภาษาไทย ฟอร์น</div>
-    </h1>
+    <Products />
   </div>
 </template>
